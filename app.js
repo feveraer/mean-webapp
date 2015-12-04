530f0610d9cdd3506a5390f51890d3b44624cd01
feveraer <frederic.everaert@gmail.com>
2015-12-04 22:49:25 +0100

    - mongoose, mongoDB setup - mongoose Schemas for user and post

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var bodyParser = require('body-parser');
 
 var api = require('./routes/api');
 var authenticate = require('./routes/authenticate')(passport);
-
+var mongoose = require('mongoose');
+//connect to mongoDB
+mongoose.connect('mongodb://localhost/shoutbox');
 var app = express();
 
 // view engine setup
---
+++ models/models.js
@@ -0,0 +1,11 @@
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String, //hash created from password
+    created_at: {type: Date, default: Date.now}
+});
+
+var postSchema = new mongoose.Schema({
+    created_by: { type: Schema.ObjectId, ref: 'User' },
+    created_at: {type: Date, default: Date.now},
+    text: String
+});
