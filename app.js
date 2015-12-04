39de3107244a74cd6c9e27265fb4d6514c73d493
feveraer <frederic.everaert@gmail.com>
2015-12-04 20:23:20 +0100

    add blank api.js and authentication.js for routing

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
+//uncomment this after implementing authenticate
+//var authenticate = require('./routes/authenticate');
 
 var app = express();
 
@@ -22,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/auth', authenticate);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
