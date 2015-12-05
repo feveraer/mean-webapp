62c4ff47c2a27e7802c478f27ed1d2214a88572e
feveraer <frederic.everaert@gmail.com>
2015-12-05 02:29:08 +0100

    implemented login and register functions in authController

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -38,12 +38,28 @@ app.controller('authController', function($scope, $http, $rootScope, $location){
   $scope.error_message = '';
 
   $scope.login = function(){
-    //placeholder until authentication is implemented
-    $scope.error_message = 'login request for ' + $scope.user.username;
+    $http.post('/auth/login', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
+    });
   };
 
   $scope.register = function(){
-    //placeholder until authentication is implemented
-    $scope.error_message = 'registeration request for ' + $scope.user.username;
+    $http.post('/auth/signup', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
+    });
   };
 });
