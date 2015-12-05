2882cf3d1af9dade29205dcf6859a5dc65a8a134
feveraer <frederic.everaert@gmail.com>
2015-12-05 01:22:52 +0100

    add ngRoute

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,4 +1,23 @@
-var app = angular.module('Shoutbox', []);
+var app = angular.module('Shoutbox', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
+});
 
 app.controller('MainCtrl', [
 	'$scope',
